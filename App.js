929fe72d5848167c0cfa8aa9a240cf6fa07a7d81
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-04-28 21:51:20 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Header from './src/components/Header';
+import PeopleList from './src/components/PeopleList';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import axios from 'axios';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      peoples: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+        .get('https://randomuser.me/api/?nat=br&results=5')
+        .then(response => {
+          const {results} = response.data;
+          this.setState({
+              peoples: results
+          });
+        });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas"/>
+        <PeopleList peoples={this.state.peoples}/>
+      </View>
+    );
+  }
+}
---
+++ src/components/PeopleListItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+import { capitalizeFirstLetter } from '../util';
+
+const PeopleListItem = props => {
+    const { people } = props;
+    const { title, first, last } = people.name;
+
+    return (
+        <View style={styles.line}>
+            <Image 
+                style={styles.avatar}
+                source={{ uri: people.picture.thumbnail }}/>
+            <Text style={styles.lineText}>
+                {`${
+                    capitalizeFirstLetter(title)
+                } ${
+                    capitalizeFirstLetter(first)
+                } ${
+                    capitalizeFirstLetter(last)
+                }`}
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    line: {
+        height: 60,
+        borderBottomWidth: 1,
+        borderBottomColor: '#bbb',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    lineText: {
+        fontSize: 20,
+        paddingLeft: 15
+    },
+    avatar: {
+        aspectRatio: 1,
+        width: 40
+    }
+});
+
+export default PeopleListItem;
